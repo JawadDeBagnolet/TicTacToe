39c85d230f4c3cc9cdb5066ddfe865d32dbe0dd0
Jawad AOUZAL <AOUZAL_Ja@lprs.local>
2024-12-12 14:36:10 +0100

    Feat(Main.java):Saisie des Pseudo des deux joueurs
    Joueur 1 à pour Pseudo la variable "nameJUn"
    Joueur 2 à pour Pseudo la variable "nameJDeux"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.println("Bienvenue sur TicTacToe !");
+        System.out.println("Joueur 1, Saisissez votre Pseudo : ");
+        String nameJUn = "";
+        nameJUn = clavier.nextLine();
+        System.out.println("Joueur 2, Saisissez votre Pseudo : ");
+        String nameJDeux = "";
+        nameJDeux = clavier.nextLine();
+
+
+
+
+
+
+
+
+
+
     }
 }
